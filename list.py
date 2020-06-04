b32450e83743197f6a9445b8892a55399a903d32
vivekagarwal1710 <66418492+vivekagarwal1710@users.noreply.github.com>
2020-06-04 17:33:46 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,4 +3,4 @@ list1 = [-5,48,33,-44,48,-483]
 
 list2= list(filter(lambda x: (x >= 0), list1)) 
   
-print("Positive numbers in the list:\n", *list2)  
+print("Positive numbers in the list:\n", list2)  
